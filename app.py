5440685bbf4386a63d65756f8416b8d46dd31651
mikebyang <michael.yang718@gmail.com>
2024-05-06 00:01:06 -0400

    missing line
    add in missing line in code that was not detected in initial commit

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ browser = webbrowser.get()
 backend = Thread(target=app.run, kwargs=APPCONFIG, daemon=False)
 
 if __name__ == '__main__':
+    backend.start()
     browser.open('https://127.0.0.1:8100')
     backend.join()
